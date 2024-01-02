a815fc463ac69f111e49bc7d2db4bfe4ec5bad95
jubayer1431 <mdhridoy625@gmail.com>
2024-01-02 20:51:17 +0600

    Set up Authentication with Clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,15 +1,26 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { Button } from '../ui/button';
 
 const Header = () => {
   return (
-    <header className={'w-full border-b'}>
+    <header className={'w-full border-b bg-black'}>
       <div className='wrapper flex items-center justify-between'>
         <Link href={'/'} className={'w-36'}>
-          <Image src={'/assets/images/logo.svg'} width={78} height={38} alt={'Logo'} />
+          <Image src={'/assets/images/logo.svg'} width={128} height={38} alt={'Logo'} />
         </Link>
-        <div className='flex w-32 justify-end gap-3'></div>
+        <div className='flex w-32 justify-end gap-3'>
+          <SignedIn>
+            <UserButton afterSignOutUrl={'/'} />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className={'rounded-full'} size={'lg'}>
+              <Link href={'/sign-in'}>Sign In</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
