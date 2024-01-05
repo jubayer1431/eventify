08dda12fdae218e62803c5a6478f4fcb540f6444
jubayer1431 <mdhridoy625@gmail.com>
2024-01-05 23:09:45 +0600

    Footer element created

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={'bg-black'}>
+      <div className='flex-center wrapper flex flex-between flex-col gap-4 p-5 text-center sm:flex-row text-[#fff]'>
+        <Link href={'/'}>
+          <Image src={'/assets/images/logo.svg'} alt={'Logo'} width={128} height={38} />
+        </Link>
+        <p> &copy; {new Date().getFullYear()} Eventify. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
