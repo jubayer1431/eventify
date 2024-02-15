a296b3dc2e91a8c0c02b43a3b75af81b97cc0af2
jubayer1431 <mdhridoy625@gmail.com>
2024-02-15 14:49:59 +0600

    Event Details page implemented

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -3,9 +3,15 @@
 import { CreateEventParams } from '@/types';
 import { handleError } from '../utils';
 import { databaseConnection } from '../database';
-import { getUserById } from './user.actions';
 import User from '../database/models/user.model';
 import Event from '../database/models/event.model';
+import Category from '../database/models/category.model';
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+    .populate({ path: 'category', model: Category, select: '_id name' });
+};
 
 export const createEvent = async ({ event, userId, path }: CreateEventParams) => {
   try {
@@ -24,6 +30,21 @@ export const createEvent = async ({ event, userId, path }: CreateEventParams) =>
     });
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await databaseConnection();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error('Event not found');
+    }
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
