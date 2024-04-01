71e7ff0de8367b8ff5962c7ec556ba9067ecfe45
jubayer1431 <mdhridoy625@gmail.com>
2024-04-01 06:49:07 +0600

    Search functionality  Implemented

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,9 +1,37 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { Input } from '../ui/input';
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
+import { useRouter, useSearchParams } from 'next/navigation';
 
-const Search = ({ placeholder }: any) => {
+const Search = ({ placeholder = 'Search Title...' }: { placeholder?: string }) => {
   const [query, setQuery] = useState('');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl;
+      if (query) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'query',
+          value: query,
+        });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ['query'],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+
+      return () => clearTimeout(delayDebounceFn);
+    }, 300);
+  }, [query, searchParams, router]);
 
   return (
     <div
@@ -11,7 +39,15 @@ const Search = ({ placeholder }: any) => {
         'flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2'
       }
     >
-      Search{placeholder}
+      <Image src={'/assets/icons/search.svg'} alt={'Search'} width={24} height={24} />
+      <Input
+        type={'text'}
+        placeholder={placeholder}
+        onChange={(e) => setQuery(e.target.value)}
+        className={
+          'p-regular-16 border-0 bg-gray-50 outline-offset-0 placeholder:text-gray-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-black'
+        }
+      />
     </div>
   );
 };
